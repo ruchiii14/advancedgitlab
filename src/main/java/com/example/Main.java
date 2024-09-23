e7b6d8e329013a693b318d7dc5a6edf5a865cc9a
Ruchika Ahuja <ruchikaahujapurswani@gmail.com>
2024-09-23 16:32:57 +0200

    add time

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,11 @@
 package com.example;
 
+import java.util.Date;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Date date = new Date();
+        System.out.println(date.toString());
+
     }
 }
