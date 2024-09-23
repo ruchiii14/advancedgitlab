1808dab3531cc5005147f05c011686a37f6039b2
Ruchika Ahuja <ruchikaahujapurswani@gmail.com>
2024-09-23 17:40:21 +0200

    main

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,10 +1,16 @@
 package com.example;
-
 import java.util.Date;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         Date date = new Date();
         System.out.println(date.toString());
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("Hola " + name);
+        scanner.close();
+
     }
+
 }
